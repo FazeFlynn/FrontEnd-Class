490659ba7a5f03aafa3ef762c734c430b0c16671
islamkhan001 <faiz.14a@gmail.com>
2024-09-26 11:52:07 +0530

    committed all files

---
+++ dom/script.js
@@ -0,0 +1,31 @@
+let a = 10;
+let b = 20;
+
+let w = screen.availWidth;
+let h = screen.availHeight;
+
+alert("Screen Size : " + w + " x " + h);
+
+// alert(window.navigator.userAgent);
+
+// let x = parseInt(prompt("Enter first num : "))
+// let y = parseInt(prompt("Enter second num : "))
+// alert(x)
+
+// let num = parseInt(prompt("Enter num : "))
+
+// if(num%2==0){
+//     alert("The number is even")
+// } else {
+//     alert("The number is odd")
+
+// }
+
+// alert("Sum : " + (x+y));
+
+function test(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    alert("Username : " + username +  "\n" + "Password : " + password);
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+strong and emphasize tag alt tag in img(it should be related to the img) is preffered for seo
+
+swayam and aicte for internship and certification
+
